a14531c3e49099d8cae46c8d3ef31e491ec1a5f2
Baptista <joaobfreitas09@gmail.com>
2020-12-22 12:58:07 +0000

    Atualizado reprodutor e reponsividade

--- projeto/livro_3/js/livro_3.js
+++ projeto/livro_3/js/livro_3.js
@@ -7,28 +7,69 @@ flipbook.turn({
     acceleration: true,
     gradients: true,
     autoCenter: false,
-    elevation: 50
+    elevation: 50,
+    when:{
+        turned: function(event, page, pageObject) {
+			atualizarProgressoReprodutor();
+        }
+    }
 });
 
 function atualizarTamanhoLivro(){
     var referencia = document.getElementById("referencia");
     var molduraLivro = document.getElementById("divMeio");
     molduraLivro.style.backgroundSize = "100% 100%";
-    flipbook.turn('size', "100%", $(window).height()*0.73);
+    flipbook.turn('size', "100%", $(window).height()*0.65);
+}
+
+function atualizarTamanhoReprodutor(){
+    var reprodutor = document.getElementById("reprodutor");
+    reprodutor.setAttribute("style", "height: " + 100 + "%;")
+    var botao = document.getElementById("botaoPlay");
+    botao.setAttribute("style", "width: " + $(window).height() * 0.1 + "px;")
+}
+
+function atualizarTamanhoProgressBar(){
+    var progressBar = document.getElementById("botaoPlay");
+    botao.setAttribute("style", "width: " + $(window).height() * 0.1 + "px;")
+}
+
+function atualizarTamanhoBotaoSair(){
+    var botao = document.getElementById("botaoSair");
+    botao.setAttribute("style", "width: " + $(window).height() * 0.05 + "px;")
 }
 
 function avancarPagina(){
     flipbook.turn('next');
+    atualizarProgressoReprodutor();
+}
+
+function atualizarProgressoReprodutor(){
+    var numeroTotalPaginas = flipbook.turn('pages');
+    var paginaAtual = flipbook.turn('page');
+    var progressoAtual = (paginaAtual * 100) / numeroTotalPaginas;
+    var barraProgresso = document.getElementById("barraProgresso");
+    barraProgresso.setAttribute("style", "width: " + progressoAtual + "%;");
 }
 
 function voltarPagina(){
     flipbook.turn('previous');
+    atualizarProgressoReprodutor();
 }
 
 $(window).on('resize',function() {
     atualizarTamanhoLivro()
+    atualizarTamanhoReprodutor()
+    atualizarTamanhoBotaoSair()
+    atualizarProgressoReprodutor()
 }).on('orientationchange', function() {
     atualizarTamanhoLivro()
+    atualizarTamanhoReprodutor()
+    atualizarTamanhoBotaoSair()
+    atualizarProgressoReprodutor()
 }).on('load', function() {
     atualizarTamanhoLivro()
+    atualizarTamanhoReprodutor()
+    atualizarTamanhoBotaoSair()
+    atualizarProgressoReprodutor()
 });
