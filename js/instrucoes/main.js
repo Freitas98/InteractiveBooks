287965e68a8195748db94c07267f33bb04d303a3
Baptista <joaobfreitas09@gmail.com>
2021-01-12 23:38:51 +0000

    Adicionado alerta na página das instruções

--- js/instrucoes/main.js
+++ js/instrucoes/main.js
@@ -1,6 +1,6 @@
 function atualizarTamanhoBotaoSair(){
     var botao = document.getElementById("botaoSair");
-    botao.setAttribute("style", "width: " + $(window).height() * 0.1 + "px;")
+    botao.setAttribute("style", "width: " + $(window).height() * 0.065 + "px;")
 }
 
 $(window).on('resize',function() {
